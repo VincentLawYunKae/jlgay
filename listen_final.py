e6a8bcee0fc88068930cc22b054aedb7ff32630c
lawyunkaevincent <126251572+lawyunkaevincent@users.noreply.github.com>
2024-10-01 03:59:51 +0800

    Update listen_final.py

--- listen_final.py
+++ listen_final.py
@@ -82,16 +82,16 @@ def handle_mode1():
             motion = "stop"
         finally:
             if motion == "forward":
-                # pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.48,0.50), starting_output=linear_speed)
+                pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.47,0.50), starting_output=linear_speed)
                 pid_right =  PID(kp_lin, ki_lin, kd_lin, setpoint=right_disp, output_limits=(0.48,0.75), starting_output=linear_speed)
                 while (left_encoder.value < abs(left_disp) - linear_tolerance) and (right_encoder.value < abs(right_disp) - linear_tolerance):
-                    # pid_left.setpoint = right_encoder.value
+                    pid_left.setpoint = right_encoder.value
                     pid_right.setpoint = left_encoder.value
                     print(f"Setpoint: {left_encoder.value}, {right_encoder.value}")
                     right_speed = pid_right(right_encoder.value)
-                    # left_speed = pid_left(left_encoder.value)
-                    print(f"Speed: {linear_speed}, {right_speed}")
-                    pibot.value = (linear_speed, right_speed)
+                    left_speed = pid_left(left_encoder.value)
+                    print(f"Speed: {left_speed}, {right_speed}")
+                    pibot.value = (left_speed, right_speed)
                 pibot.value = (0, 0)
             elif motion == "backward":
                 pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.48,0.52), starting_output=linear_speed)
