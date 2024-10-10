3503641219cf4376c8a3d11d1e67fdfd9906c0d5
lawyunkaevincent <126251572+lawyunkaevincent@users.noreply.github.com>
2024-10-10 13:30:31 +0800

    Update listen_m23.py

--- listen_m23.py
+++ listen_m23.py
@@ -126,7 +126,7 @@ def move():
         motion = 'stop'
     elif (left_speed != right_speed ):
         global turn_motion_queue
-        turn_motion_queue.append(left_speed, right_speed)
+        turn_motion_queue.append((left_speed, right_speed))
         motion = 'turning'
     elif (left_speed > 0 and right_speed > 0):
         motion = 'forward'
