54b71ff658a8d4bcb50ce4d89b46eb959e466a7a
lawyunkaevincent <126251572+lawyunkaevincent@users.noreply.github.com>
2024-10-13 23:52:57 +0800

    Update listen_m23.py

--- listen_m23.py
+++ listen_m23.py
@@ -176,6 +176,7 @@ def set_dt():
     # Try to retrieve the dt_left and dt_right parameters from the GET request
     dt_left = float(request.args.get('dt_left', 0))  # Default to 0 if not provided
     dt_right = float(request.args.get('dt_right', 0)) 
+    return str(dt_left) + " " + str(dt_right)
 
 @app.route('/angle')
 def set_angle():
